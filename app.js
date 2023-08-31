050240a8dab4b1020fbf91c025bc86fbf905882e
Ederson Torresini <boidacarapreta@gmail.com>
2023-08-31 18:00:45 +0000

    Rodar ESLint

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', (req, res) => res.type('html').send(html))
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+server.keepAliveTimeout = 120 * 1000
+server.headersTimeout = 120 * 1000
 
 const html = `
 <!DOCTYPE html>
