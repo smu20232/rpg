002d6cda57ed3ac2113e4fe73cafd2a12bd3c9d9
Mike Domingues <mikeicd@gmail.com>
2023-09-13 21:46:21 -0300

    Adicionado teste de redirecionamento pelo servidor

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const port = process.env.PORT || 3001
 const socketIo = require('socket.io');
 
 
-app.get('/styles.css', (req, res) => {
-    res.type('text/css').sendFile(path.join(__dirname, '/client/styles.css'));
-});
-app.get('/', (req, res) => res.type('html').sendFile(path.join(__dirname, '/client/index.html')))
-app.get('/home', (req, res) => res.type('html').sendFile(path.join(__dirname, '/client/home.html')))
-app.get('/main.js', (req, res) => {
-    res.type('application/javascript').sendFile(path.join(__dirname, '/client/main.js'));
-});
+app.get('/', (req, res) => res.type('html').sendFile(path.join(__dirname, '/client/index.html')));
+app.get('/home', (req, res) => res.type('html').sendFile(path.join(__dirname, '/client/home.html')));
+app.get('/main.js', (req, res) => res.type('application/javascript').sendFile(path.join(__dirname, '/client/main.js')));
+app.get('/styles.css', (req, res) => res.type('text/css').sendFile(path.join(__dirname, '/client/styles.css')));
+app.get('/sala', (req, res) => res.type('html').sendFile(path.join(__dirname, '/client/sala.html')));
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 const io = socketIo(server);
@@ -22,8 +20,12 @@ server.headersTimeout = 120 * 1000
 
 io.on('connection', (socket) => {
     console.log('A client connected');
+
     socket.on('register', (registerData) => {
         console.log(registerData)
+        if (registerData.email){
+            res.redirect('/sala');
+        }
     });
 
     socket.on('disconnect', () => {
